ec10a9a9e679d31deb34a5fdd903ff76695ca423
dojyj <dojyj@naver.com>
2021-07-24 11:13:05 +0900

    ESLint && Prettier 적용

--- routes/commons.js
+++ routes/commons.js
@@ -1,19 +1,19 @@
-const firebaseAdmin = require("firebase-admin");
-const { secrets } = require("../config");
+const firebaseAdmin = require('firebase-admin');
+const { secrets } = require('../config');
 // const nodemailer = require("nodemailer");
 // const crypto = require("crypto");
 
 const ERRORS = {
   AUTH: {
-    TOKEN_FAIL: "TOKEN_FAIL",
-    NO_AUTH_IN_HEADER: "NO_AUTH_IN_HEADER",
-    NO_UID: "NO_UID",
-    CANT_SIGNUP: "CANT_SIGNUP",
-    ALREADY_SIGNEDUP: "ALREADY_SIGNEDUP",
-    NO_PERMISSION: "NO_PERMISSION",
+    TOKEN_FAIL: 'TOKEN_FAIL',
+    NO_AUTH_IN_HEADER: 'NO_AUTH_IN_HEADER',
+    NO_UID: 'NO_UID',
+    CANT_SIGNUP: 'CANT_SIGNUP',
+    ALREADY_SIGNEDUP: 'ALREADY_SIGNEDUP',
+    NO_PERMISSION: 'NO_PERMISSION',
   },
   DATA: {
-    INVALID_DATA: "INVALID_DATA",
+    INVALID_DATA: 'INVALID_DATA',
   },
 };
 
@@ -24,29 +24,29 @@ firebaseAdmin.initializeApp({
 const firestore = firebaseAdmin.firestore;
 
 const DB = {
-  users: firestore().collection("users"),
-  auctionInfo:firestore().collection("auctionInfo"),
-}
+  users: firestore().collection('users'),
+  auctionInfo: firestore().collection('auctionInfo'),
+};
 
 const tokenExporter = (headers) => {
-    if (headers.authorization) {
-      let idToken = headers.authorization.split("Bearer ")[1];
-      return firebaseAdmin
-        .auth()
-        .verifyIdToken(idToken)
-        .then((decodedToken) => decodedToken)
-        .catch((_err) => {
-          return ERRORS.AUTH.TOKEN_FAIL;
-        });
-    } else {
-      return ERRORS.AUTH.NO_AUTH_IN_HEADER;
-    }
-  };
+  if (headers.authorization) {
+    let idToken = headers.authorization.split('Bearer ')[1];
+    return firebaseAdmin
+      .auth()
+      .verifyIdToken(idToken)
+      .then((decodedToken) => decodedToken)
+      .catch((_err) => {
+        return ERRORS.AUTH.TOKEN_FAIL;
+      });
+  } else {
+    return ERRORS.AUTH.NO_AUTH_IN_HEADER;
+  }
+};
 
 module.exports = {
   DB,
   firestore,
   firebaseAdmin,
   ERRORS,
-  tokenExporter
-}; 
+  tokenExporter,
+};
