034065629708a6449b74b13a1df74e8e216d7d18
dojyj <dojyj@naver.com>
2021-07-31 13:02:36 +0900

    Jest Testing Framework 적용

--- server.js
+++ server.js
@@ -58,9 +58,11 @@ app.use(function (err, req, res, next) {
   res.render('error.html');
 });
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
   // truffle_connect.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"));
 
   console.log('Express Listening at http://localhost:' + port);
 });
+
+module.exports = server;
